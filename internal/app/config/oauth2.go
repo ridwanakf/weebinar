9dc3f12fd002eb2f51915433db79d162304b88a5
ridwanakf <blog.afwan@gmail.com>
2020-12-14 10:30:59 +0700

    add oauth to config

--- internal/app/config/oauth2.go
+++ internal/app/config/oauth2.go
@@ -1,7 +1,7 @@
 package config
 
 type OAuth2Cred struct {
-	ClientID     string `env:"OAUTH_CLIENT_ID"`
-	ClientSecret string `env:"OAUTH_CLIENT_SECRET"`
-	RandomState  string `env:"OAUTH_RANDOM_STATE"`
+	ClientID     string `yaml:"client_id" env:"OAUTH_CLIENT_ID"`
+	ClientSecret string `yaml:"client_secret" env:"OAUTH_CLIENT_SECRET"`
+	RandomState  string `yaml:"random_state" env:"OAUTH_RANDOM_STATE"`
 }
